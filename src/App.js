c0186925b78ee90d97a4715cff53ba198fca9650
Manumi-0728 <deillvarmm.20@uom.lk>
2023-12-28 21:10:27 +0530

    modify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'h8k-components';
 
 import Articles from './components/Articles';
 
-const title = "Sorting Articles";
+const title = 'Sorting Articles';
 
-function App({articles}) {
+function App({ articles }) {
+    const [currentArticles, setCurrentArticles] = useState(articles);
+
+    const handleMostUpvoted = () => {
+        const sortedByUpvotes = [...currentArticles].sort((a, b) => b.upvotes - a.upvotes);
+        setCurrentArticles(sortedByUpvotes);
+    };
+
+    const handleMostRecent = () => {
+        const sortedByDate = [...currentArticles].sort((a, b) => new Date(b.date) - new Date(a.date));
+        setCurrentArticles(sortedByDate);
+    };
 
     return (
         <div className="App">
             <h8k-navbar header={title}></h8k-navbar>
             <div className="layout-row align-items-center justify-content-center my-20 navigation">
                 <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
+                <button data-testid="most-upvoted-link" className="small" onClick={handleMostUpvoted}>
+                    Most Upvoted
+                </button>
+                <button data-testid="most-recent-link" className="small" onClick={handleMostRecent}>
+                    Most Recent
+                </button>
             </div>
-            <Articles articles={articles}/>
+            <Articles articles={currentArticles} />
         </div>
     );
-
 }
 
 export default App;
+
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,61 +1,40 @@
 import React, { useState } from 'react';
 
 function Articles({ articles }) {
-  const [sortedBy, setSortedBy] = useState('upvotes'); // State to track sorting type
+    const [sortedArticles, setSortedArticles] = useState(articles);
 
-  // Function to sort articles based on upvotes or date
-  const sortArticles = (type) => {
-    setSortedBy(type);
-  };
+    const sortByUpvotes = () => {
+        const sortedByUpvotes = [...articles].sort((a, b) => b.upvotes - a.upvotes);
+        setSortedArticles(sortedByUpvotes);
+    };
 
-  // Sort articles based on the chosen criteria
-  const sortedArticles = [...articles].sort((a, b) => {
-    if (sortedBy === 'upvotes') {
-      return b.upvotes - a.upvotes; // Sort by upvotes in descending order
-    } else if (sortedBy === 'date') {
-      return new Date(b.date) - new Date(a.date); // Sort by date in descending order
-    }
-    return 0;
-  });
+    const sortByDate = () => {
+        const sortedByDate = [...articles].sort((a, b) => new Date(b.date) - new Date(a.date));
+        setSortedArticles(sortedByDate);
+    };
 
-  return (
-    <div className="card w-50 mx-auto">
-      <div className="btn-group" role="group">
-        {/* Buttons to sort articles */}
-        <button
-          onClick={() => sortArticles('upvotes')}
-          data-testid="most-upvoted-link"
-        >
-          Most Upvoted
-        </button>
-        <button
-          onClick={() => sortArticles('date')}
-          data-testid="most-recent-link"
-        >
-          Most Recent
-        </button>
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Upvotes</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* Render articles dynamically */}
-          {sortedArticles.map((article, index) => (
-            <tr key={`article-${index}`} data-testid="article">
-              <td data-testid="article-title">{article.title}</td>
-              <td data-testid="article-upvotes">{article.upvotes}</td>
-              <td data-testid="article-date">{article.date}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    return (
+        <div className="card w-50 mx-auto">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Upvotes</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {sortedArticles.map((article, index) => (
+                        <tr data-testid={`article-${index}`} key={`article-${index}`}>
+                            <td data-testid={`article-title-${index}`}>{article.title}</td>
+                            <td data-testid={`article-upvotes-${index}`}>{article.upvotes}</td>
+                            <td data-testid={`article-date-${index}`}>{article.date}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default Articles;
